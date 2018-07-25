5865394073bcc1a2cb4fa0e0b2219835da296bd8
Piotr Kabaciński <pkabacinskicq@MacBook-Pro-Piotr.local>
2018-07-25 12:35:58 +0200

    first

---
+++ README.md
@@ -0,0 +1,25 @@
+# Pug/Unpug
+
+```bash
+npm i
+```
+
+```bash
+npm run unpug # changes all *.pug extensions to *.pxx` in ./src
+```
+
+```bash
+npm run unpug bar.pug # changes all *.pug extensions to *.pxx in ./src except ./src/bar.pug
+```
+
+```bash
+npm run repug # restore all *.pxx ext to *.pug` in ./src
+```
+
+## Config
+
+Default `*.pug` files directory is declared in `dir` variable in `unpug.sh:6`, default: `./src/`.
+
+## Pre-commit
+
+After commit `*.pxx` files are automatically changed to `.pug` thanks to pre-commit indicator (`package.json:16`)
---
+++ unpug.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# sh change.sh --unpug foo.pug # unpug all files except foo.pug
+# sh chage.sh --repug # restore all pug files
+
+dir="./src/"
+
+function modify {
+    fromExt=$1
+    toExt=$2
+    except=$3
+
+    for file in $dir*.$fromExt; do
+        if [[ ! "$file" == "$dir$except" ]]; then
+            mv $file $dir`basename $file .$fromExt`.$toExt;
+        fi
+    done
+
+    echo "\033[1;32mFiles extension modified from ."$1 "to ."$2 "in" $dir "\033[0m";
+
+    if [[ $except != "" && -f $dir$except ]]; then
+        echo "\033[1;32m...except" $dir$except "\033[0m";
+    fi
+}
+
+if [ "$1" == "" ]; then
+    echo "Argument missing, don't know what to do :("
+else
+    case "$1" in
+        "--unpug")
+            read -p "Are you sure you want to unpug? (y/n)" choice
+            case "$choice" in
+                y|Y ) modify "pug" "pxx" "$2";;
+                n|N ) ;;
+                * ) echo "Invalid value";;
+            esac
+            ;;
+        "--repug")
+            echo "Repugging..."
+            modify "pxx" "pug"
+            ;;
+        *)
+            echo "'$1' is not a valid flag"
+            ;;
+    esac
+fi
